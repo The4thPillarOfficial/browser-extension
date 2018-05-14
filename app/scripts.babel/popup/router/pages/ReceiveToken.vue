60c85bfd7bc47e79e00ae10b47a0a26c1cebeba1
Denis Jazbec <denis.jazbec@smartee.io>
2018-05-14 12:13:45 +0100

    Add QR code to receive token page

--- app/scripts.babel/popup/router/pages/ReceiveToken.vue
+++ app/scripts.babel/popup/router/pages/ReceiveToken.vue
@@ -3,9 +3,7 @@
         <popup-header/>
         <div class="container">
             <h1>Receive Four</h1>
-            <div class="qr-code">
-                <!-- TODO:: QR code of wallet -->
-            </div>
+            <div id="qr-code" class="qr-code"></div>
             <div class="wallet-address">{{ wallet.defaultAccount }}</div>
             <div class="note">Share this address to receive payments</div>
         </div>
@@ -14,12 +12,22 @@
 
 <script>
     import {mapState} from 'vuex';
+    import qrcode from 'qrcode-generator';
 
     export default {
         computed: {
             ...mapState([
                 'wallet',
             ]),
+        },
+        mounted() {
+            let typeNumber = 10;
+            let errorCorrectionLevel = 'H';
+            let qr = qrcode(typeNumber, errorCorrectionLevel);
+
+            qr.addData(this.wallet.defaultAccount);
+            qr.make();
+            document.getElementById('qr-code').innerHTML = qr.createSvgTag();
         },
         methods: {
         }
